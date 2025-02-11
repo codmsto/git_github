a9e6cd3b57a45e9c612334390b59869918d7d7cf
codmsto <silverbird0920@gmail.com>
2025-02-11 22:43:38 +0900

    fix

--- basic_syntax/1.basic_command.sh
+++ basic_syntax/1.basic_command.sh
@@ -6,3 +6,39 @@ git commit -m "메시지"
 
 # 원격 repo에 main브랜치 변경사항(commitID)을 업로드
 git push origin main
+
+# 현재 working directory, staging area의 상태 조회
+git status
+
+# 특정 파일만 add할 경우
+git add 특정파일(위치포함)
+
+# local repository에 커밋이력 생성
+git commit -m "메시지타이틀" -m "메시지내용"
+
+# vi에디터 모드에서 작성 : 첫줄에는 제목, 두번째줄부터 내용을
+git commit
+
+# add와 commit을 동시에
+git commit -am "add와 commit을 동시에"
+
+# local repo의 commit 이력 조회
+git log
+
+# git 로그를 간결하게 조회
+git log --oneline
+
+# git 로그를 그래프형태로 조회
+git log --graph
+
+# main브랜치 뿐만아니라 전체 commit 이력 조회
+git log --all  
+
+# 충돌무시하고 강제 push할때
+git push origin main --force
+
+# 특정 commit ID로의 전환(소스코드를 과거의 특정 버전으로 되돌릴때)
+git checkout 커밋ID
+
+# 다시 현재의 브랜치의 commit 되돌아올때
+git checkout 현재브랜치명(main)
